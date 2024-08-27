c2417a2ee60a2d016504bda9514a8721761bf5fe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-27 16:38:56 +0200

    add Parameter class

---
+++ app/src/main/java/com/klodnicki/watermeter/model/Parameter.java
@@ -0,0 +1,31 @@
+package com.klodnicki.watermeter.model;
+
+public class Parameter {
+    private String name;
+    private String type;
+    private boolean required;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+}
