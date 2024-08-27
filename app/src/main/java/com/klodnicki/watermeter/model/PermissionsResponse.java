485f5eac3682391e4f86ce845e1d54291b7a1af6
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-27 16:41:52 +0200

    add PermissionsResponse

---
+++ app/src/main/java/com/klodnicki/watermeter/model/PermissionsResponse.java
@@ -0,0 +1,15 @@
+package com.klodnicki.watermeter.model;
+
+import java.util.Map;
+
+public class PermissionsResponse {
+    private Map<String, Group> groups;
+
+    public Map<String, Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Map<String, Group> groups) {
+        this.groups = groups;
+    }
+}
