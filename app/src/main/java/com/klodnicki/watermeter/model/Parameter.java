811c843a8a0b08ad9b13d0d3f50d4562b1354beb
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-28 11:35:51 +0200

    refactor Parameter class with required fields

--- app/src/main/java/com/klodnicki/watermeter/model/Parameter.java
+++ app/src/main/java/com/klodnicki/watermeter/model/Parameter.java
@@ -3,8 +3,13 @@ package com.klodnicki.watermeter.model;
 public class Parameter {
     private String name;
     private String type;
+    private String label;
     private boolean required;
-
+    private String value;
+    private Integer min;   // for int type
+    private Integer max;   // for int type
+    private String regexp; // for validation
+    
     public String getName() {
         return name;
     }
@@ -21,11 +26,51 @@ public class Parameter {
         this.type = type;
     }
 
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
     public boolean isRequired() {
         return required;
     }
 
     public void setRequired(boolean required) {
         this.required = required;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+
+    public String getRegexp() {
+        return regexp;
+    }
+
+    public void setRegexp(String regexp) {
+        this.regexp = regexp;
     }
 }
