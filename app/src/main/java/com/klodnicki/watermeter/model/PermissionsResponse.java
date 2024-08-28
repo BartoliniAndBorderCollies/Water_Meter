385d5f3c0173b5bae103446edbe433bd8614df78
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-28 23:00:06 +0200

    initialize HashMap in PermissionsResponse

--- app/src/main/java/com/klodnicki/watermeter/model/PermissionsResponse.java
+++ app/src/main/java/com/klodnicki/watermeter/model/PermissionsResponse.java
@@ -1,12 +1,17 @@
 package com.klodnicki.watermeter.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PermissionsResponse {
-    private Map<String, Group> groups;
+    private Map<String, Group> groups = new HashMap<>();
 
     public Map<String, Group> getGroups() {
         return groups;
     }
 
+    public void setGroups(Map<String, Group> groups) {
+        this.groups = groups;
+    }
 }
+
