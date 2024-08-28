c81f735af4d43225d32a6e24a4a86168ec2f635a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-28 23:01:37 +0200

    add logginIntereptor in ApiClient

--- app/src/main/java/com/klodnicki/watermeter/api/ApiClient.java
+++ app/src/main/java/com/klodnicki/watermeter/api/ApiClient.java
@@ -1,5 +1,7 @@
 package com.klodnicki.watermeter.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,8 +12,16 @@ public class ApiClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
